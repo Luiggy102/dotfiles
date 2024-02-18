d9c52934eb94ef69b5a041f239a869dcabfb8218
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2024-02-17 22:58:39 -0500

    added launcher script and more audio keys

---
+++ .scripts/launcher.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+choises="pomofocus
+github
+vw
+vimwiki
+anki
+zathura
+songsterr
+documentos
+descargas
+linkedin
+monkeyType
+kitty
+invidious
+symbolab
+geogebra
+pdf
+protonMail
+whatsapp
+calculadoraDeSueño
+deepl
+simpleLogin
+googleAcademico
+telegram
+pictures
+videos
+editScript"
+
+chosen=$(echo "$choises" | fuzzel -d)
+
+case "$chosen" in
+	
+    # -- Folders -- #
+    documentos ) dolphin $HOME/Documents/ ;;
+    pictures ) dolphin $HOME/Pictures ;;
+    descargas ) dolphin $HOME/Downloads ;;
+    videos ) dolphin $HOME/Videos ;;
+	
+    # -- Apps -- #
+	firefox ) firefox ;;
+    kitty ) kitty ;; 
+	zathura ) zathura ;;
+	pdf ) zathura ;;
+    keepassXC ) keepassxc ;;
+	
+
+    # -- wiki -- #
+    vw ) kitty nvim -c "VimwikiIndex" ;;
+    vimwiki ) kitty nvim -c "VimwikiIndex" ;;
+
+    # -- Links -- # 
+    songsterr ) firefox 'https://www.songsterr.com/' ;;
+    monkeyType ) firefox 'https://monkeytype.com/' ;;
+	telegram ) firefox 'https://web.telegram.org/' ;;
+    anki ) firefox 'https://ankiweb.net/decks' ;;
+    github ) firefox 'https://github.com/Luiggy102?tab=repositories' ;;
+    deepl ) firefox 'https://www.deepl.com/translator' ;;
+    simpleLogin ) firefox 'https://app.simplelogin.io/dashboard/' ;;
+	invidious ) firefox 'https://invidious.io.lol/' ;;
+	geogebra ) firefox 'https://www.geogebra.org/graphing?lang=en' ;;
+	calculadoraDeSueño ) firefox 'https://sleepytime.cc/' ;;
+	googleAcademico) firefox 'https://scholar.google.com/' ;;
+	symbolab) firefox 'https://es.symbolab.com/' ;;
+    linkedin ) firefox 'https://www.linkedin.com/feed/';;
+	whatsapp) firefox 'https://web.whatsapp.com' ;;
+	tutanota) firefox 'https://app.tuta.com/mail/' ;;
+	calendario ) firefox 'https://calendar.proton.me/u/0/' ;;
+	protonMail ) firefox 'https://mail.proton.me/u/0/inbox' ;;
+    pomofocus ) firefox 'https://pomofocus.io/' ;;
+
+    # -- editConfig -- # 
+    editScript ) kitty nvim /usr/local/bin/fzfScript ;;
+
+esac
