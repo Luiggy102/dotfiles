8197b5ef290d442197c0def3f5fdd325dc956610
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-06-07 15:40:13 -0500

    added ytfzf and zathura

--- .config/ytfzf/conf.sh
+++ .config/ytfzf/conf.sh
@@ -1,10 +1,11 @@
 # video_pref=worst
 # show_thumbnails=1
-search_region=US
+search_region=ES
 fancy_subs=1
 async_thumbnails=1
-is_loop=1
+# is_loop=1
 thumbnail_quality=medium
 thumbnail_viewer=kitty
 fzf_preview_side=right
-ytdl_pref="worst+bestaudio/best"
+#ytdl_pref="worst+bestaudio/best"
+video_pref="bestvideo[height<=?720]+bestaudio/best"
