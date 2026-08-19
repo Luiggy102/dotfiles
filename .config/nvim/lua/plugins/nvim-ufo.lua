a3d02f3701144b712858b8be07ba8bf087a85346
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2026-08-19 10:27:59 -0500

    fix buffer docs nvim

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -6,6 +6,9 @@ return {
     },
     opts = {
         provider_selector = function(bufnr, filetype, buftype)
+            if vim.bo[bufnr].buftype ~= "" then
+                return ""
+            end
             return { "treesitter", "indent" }
         end,
         close_fold_kinds_for_ft = {
